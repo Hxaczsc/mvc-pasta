bf8eede3c811f80b9128b30044290b8d907b003e
User <d1relka_pix@mail.ru>
2024-10-20 23:29:52 +0300

    классу PastaView добавлены методы, выводящие меню на экран пользователя, мелкие изменения в названиях методов класса контроллера

--- Controllers/PastaController.py
+++ Controllers/PastaController.py
@@ -2,14 +2,14 @@ class Pastacontroller:
     def __init__(self, model):
         self.model = model
 
-    def restaurant_menu(self):
+    def get_restaurant_menu(self):
         pasta_data = (f"Паста :: {self.model.get_name()}\n"
                       f"Цена :: {self.model.get_price()}\n"
                       f"Вес :: {self.model.get_weight()}\n"
                       f"Состав :: {", ".join(self.model.get_ingredients())}\n")
         return pasta_data
 
-    def web_menu(self):
+    def get_web_menu(self):
         pasta_data = (f"Паста :: {self.model.get_name()}\n"
                       f"Цена :: {self.model.get_price()}\n"
                       f"Вес :: {self.model.get_weight()}\n"
--- Views/PastaView.py
+++ Views/PastaView.py
@@ -2,3 +2,9 @@ class PastaView:
     def __init__(self, controller):
         self.controller = controller
 
+
+    def print_restaurant_menu(self):
+        print(self.controller.get_restaurant_menu)
+
+    def print_web_menu(self):
+        print(self.controller.get_web_menu)
