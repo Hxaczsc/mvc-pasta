12999d883f07a674f8b2d57318bd5a0fcbda5b1e
User <d1relka_pix@mail.ru>
2024-10-20 17:57:44 +0300

    добавлено защищенное поле picture классу PastaModel, написан геттер и сеттер поля написаны методы создания ресторанного и веб меню

--- Controllers/PastaController.py
+++ Controllers/PastaController.py
@@ -1,3 +1,19 @@
 class Pastacontroller:
     def __init__(self, model):
-        self.__model = model
+        self.model = model
+
+    def restaurant_menu(self):
+        pasta_data = (f"Паста :: {self.model.get_name()}\n"
+                      f"Цена :: {self.model.get_price()}\n"
+                      f"Вес :: {self.model.get_weight()}\n"
+                      f"Состав :: {", ".join(self.model.get_ingredients())}\n")
+        return pasta_data
+
+    def web_menu(self):
+        pasta_data = (f"Паста :: {self.model.get_name()}\n"
+                      f"Цена :: {self.model.get_price()}\n"
+                      f"Вес :: {self.model.get_weight()}\n"
+                      f"Состав :: {", ".join(self.model.get_ingredients())}\n"
+                      f"Фото :: {self.model.get_picture}")
+        return pasta_data
+
