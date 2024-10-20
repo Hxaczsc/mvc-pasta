9d14fc0758e7e21fa392aa3d7bf6c0005226286f
User <d1relka_pix@mail.ru>
2024-10-20 19:54:58 +0300

    расширены методы set для класса контроллера при помощи проверки прав доступа

--- Controllers/PastaController.py
+++ Controllers/PastaController.py
@@ -17,3 +17,21 @@ class Pastacontroller:
                       f"Фото :: {self.model.get_picture}")
         return pasta_data
 
+    def set_ingredients(self, user_rights, new_ingredients):
+        if user_rights in ["Admin", "IsStaff", "IsSuperuser"]:
+            self.model.set_ingredients(new_ingredients)
+            return "Рецепт успешно изменен"
+        else:
+            return "Нет права доступа"
+
+    def set_price(self, user_rights, new_price):
+        if user_rights in ["Admin", "IsStaff", "IsSuperuser"]:
+            self.model.set_price(new_price)
+
+    def set_weight(self, user_rights, new_weight):
+        if user_rights in ["Admin", "IsStaff", "IsSuperuser"]:
+            self.model.set_weight(new_weight)
+
+    def set_picture(self, user_rights, new_picture):
+        if user_rights in ["Admin", "IsStaff", "IsSuperuser"]:
+            self.model.set_picture(new_picture)
